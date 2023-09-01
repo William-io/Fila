9a2352c41046e1a7c52f1098dcb64c95abdb573b
RD-Ricardo <ricardodias169@gmail.com>
2023-09-01 15:17:11 -0300

    feat(back-end): master criando entidade base

---
+++ src/BuildingBlocks/NarniaSystem.ProjetoFila.Core/Domain/Entity.cs
@@ -0,0 +1,14 @@
+﻿namespace NarniaSystem.ProjetoFila.Core.Domain
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public DateTime CreatedAt { get; set; } 
+        public DateTime AlteredAt { get; set; }
+        public bool IsActive { get; private set; } = true;
+        public void ChangeIsActive(bool isActive)
+        {
+            IsActive = isActive;
+        }
+    }
+}
